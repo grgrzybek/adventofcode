07d69397945fc7d7bb3718011d9772bf9aaa4bca
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-12-23 19:03:51 +0100

    [Go] Solution for Day 22 - part 2 - should work but it doesn't

--- go/cmd/day22/day22.go
+++ go/cmd/day22/day22.go
@@ -20,6 +20,7 @@ package aoc2018
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"github.com/grgrzybek/adventofcode2018/go/cmd"
 	"github.com/spf13/cobra"
@@ -64,17 +65,22 @@ func run(cmd *cobra.Command, _ []string) {
 	scanner.Scan()
 	_, _ = fmt.Sscanf(scanner.Text(), "target: %d,%d", &target[0], &target[1])
 
-	width := target[0] + 1
-	height := target[1] + 1
+	// for part 2, we'll calculate a bit more
+
+	// width := (target[0]) + 1
+	// height := (target[1]) + 1
+	width := (target[0]) + 200
+	height := (target[1]) * 2
 	fmt.Printf("%d x %d\n", width, height)
 
-	regionEL := make([]int64, width*height)
-	regionTypes := make([]int, width*height)
+	size := (width + 1) * (height + 1)
+	regionEL := make([]int64, size)
+	regionTypes := make([]int, size)
 
 	var x int64
 	var y int64
-	for y = 0; y <= target[1]; y++ {
-		for x = 0; x <= target[0]; x++ {
+	for y = 0; y < height; y++ {
+		for x = 0; x < width; x++ {
 			var gi int64
 			if (x == 0 && y == 0) || (x == target[0] && y == target[1]) {
 				gi = 0
@@ -85,9 +91,10 @@ func run(cmd *cobra.Command, _ []string) {
 			} else {
 				gi = regionEL[(y-1)*width+x] * regionEL[y*width+x-1]
 			}
-			fmt.Printf("gi(%d, %d) = %d\n", x, y, gi)
+			// fmt.Printf("gi(%d, %d) = %d\n", x, y, gi)
 			var el int64
 			el = (gi + depth) % 20183
+			// fmt.Printf("idx: %d\n", y*width+x)
 			regionEL[y*width+x] = el
 			regionTypes[y*width+x] = int(el % 3)
 		}
@@ -97,14 +104,199 @@ func run(cmd *cobra.Command, _ []string) {
 	for y = 0; y <= target[1]; y++ {
 		for x = 0; x <= target[0]; x++ {
 			risk += regionTypes[y*width+x]
-			switch regionTypes[y*width+x] {
-			case 0: fmt.Printf(".") // rocky
-			case 1: fmt.Printf("=") // wet
-			case 2: fmt.Printf("|") // narrow
+		}
+	}
+	for y = 0; y < height; y++ {
+		for x = 0; x < width; x++ {
+			if x == 0 && y == 0 {
+				fmt.Print("X")
+			} else if x == target[0] && y == target[1] {
+				fmt.Print("T")
+			} else {
+				switch regionTypes[y*width+x] {
+				case 0:
+					fmt.Printf(".") // rocky
+				case 1:
+					fmt.Printf("=") // wet
+				case 2:
+					fmt.Printf("|") // narrow
+				}
 			}
 		}
 		fmt.Println()
 	}
 
-	fmt.Printf("Answer 1: %d, Answer 2: %d\n", risk, 0)
+	// part2
+
+	check := list.New()
+	check.PushBack([]int{0, 0, torch, 0}) // x, y, equipment, time
+	regionTimes := make([]int, size*3)
+	for i := 0; i < int(size*3); i++ {
+		regionTimes[i] = -1
+	}
+	// regionTimes[target[1]*width+target[0]] = -2
+
+	calculate(regionTypes, regionTimes, check, int(width), int(height), int(target[0]), int(target[1]))
+
+	// torch
+	distance := regionTimes[target[1]*width*3+(target[0]*3)+1]
+
+	// for y = 0; y < height; y++ {
+	// 	for x = 0; x < width-7; x ++ {
+	// 		if x == 0 && y == 0 {
+	// 			fmt.Print("[     X     ] ")
+	// 		} else if x == target[0] && y == target[1] {
+	// 			fmt.Print("[     T     ] ")
+	// 		} else {
+	// 			fmt.Printf("[%03d|%03d|%03d] ", regionTimes[y*width*3+(x*3)], regionTimes[y*width*3+(x*3)+1], regionTimes[y*width*3+(x*3)+2])
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	fmt.Printf("Answer 1: %d, Answer 2: %d\n", risk, distance)
 }
+
+func calculate(types []int, times []int, toCheck *list.List, width, height int, tx, ty int) {
+	toCheck2 := list.New()
+	for e := toCheck.Front(); e != nil; e = e.Next() {
+		if xye, ok := e.Value.([]int); ok {
+			x := xye[0] // x of current region
+			y := xye[1] // y of current region
+			e := xye[2] // equipment with which we've entered
+			d := xye[3] // time spent before entering - will increase by 1 or 8 (when switching equipment)
+
+			// equipment we can use here
+			// what's current terrain?
+			terrain := types[y*width+x]
+			switch terrain {
+			case rocky: // rocky - need torch | climbingGear
+				if e == neither {
+					d += 7
+				}
+			case wet: // wet - need neither | climbingGear
+				if e == torch {
+					d += 7
+				}
+			case narrow: // narrow - need neither | torch
+				if e == climbingGear {
+					d += 7
+				}
+			}
+
+			proceed := false
+
+			// 3 ints for each cell
+			if e == neither {
+				if times[y*width*3+(x*3)+0] == -1 || times[y*width*3+(x*3)+0] > d {
+					times[y*width*3+(x*3)+0] = d
+					proceed = true
+				}
+			} else if e == torch {
+				if times[y*width*3+(x*3)+1] == -1 || times[y*width*3+(x*3)+1] > d {
+					times[y*width*3+(x*3)+1] = d
+					proceed = true
+				}
+			} else if e == climbingGear {
+				if times[y*width*3+(x*3)+2] == -1 || times[y*width*3+(x*3)+2] > d {
+					times[y*width*3+(x*3)+2] = d
+					proceed = true
+				}
+			}
+
+			// we can go to neighbour cells with equipment permitted in current cell
+			if proceed {
+				// 1 minute for walking
+				d++
+				dneither := d
+				if e != neither {
+					dneither += 7
+				}
+				dtorch := d
+				if e != torch {
+					dtorch += 7
+				}
+				dclimbingGear := d
+				if e != climbingGear {
+					dclimbingGear += 7
+				}
+				// up
+				if y > 0 {
+					// if (times[(y-1)*width*3+(x*3)+0] == -1 || times[(y-1)*width*3+(x*3)+0] > d) {
+					if types[(y-1)*width+x] != rocky {
+						toCheck2.PushBack([]int{x, y - 1, neither, dneither})
+					}
+					if types[(y-1)*width+x] != wet {
+						toCheck2.PushBack([]int{x, y - 1, torch, dtorch})
+					}
+					if types[(y-1)*width+x] != narrow {
+						toCheck2.PushBack([]int{x, y - 1, climbingGear, dclimbingGear})
+					}
+				}
+				// down
+				if y < height {
+					if types[(y+1)*width+x] != rocky {
+						toCheck2.PushBack([]int{x, y + 1, neither, dneither})
+					}
+					if types[(y+1)*width+x] != wet {
+						toCheck2.PushBack([]int{x, y + 1, torch, dtorch})
+					}
+					if types[(y+1)*width+x] != narrow {
+						toCheck2.PushBack([]int{x, y + 1, climbingGear, dclimbingGear})
+					}
+				}
+				// left
+				if x > 0 {
+					if types[y*width+x-1] != rocky {
+						toCheck2.PushBack([]int{x - 1, y, neither, dneither})
+					}
+					if types[y*width+x-1] != wet {
+						toCheck2.PushBack([]int{x - 1, y, torch, dtorch})
+					}
+					if types[y*width+x-1] != narrow {
+						toCheck2.PushBack([]int{x - 1, y, climbingGear, dclimbingGear})
+					}
+				}
+				// right
+				if x < width {
+					if types[y*width+x+1] != rocky {
+						toCheck2.PushBack([]int{x + 1, y, neither, dneither})
+					}
+					if types[y*width+x+1] != wet {
+						toCheck2.PushBack([]int{x + 1, y, torch, dtorch})
+					}
+					if types[y*width+x+1] != narrow {
+						toCheck2.PushBack([]int{x + 1, y, climbingGear, dclimbingGear})
+					}
+				}
+			}
+			// fmt.Print("[H[2J[3J")
+			// for y = 0; y < height; y++ {
+			// 	for x = 0; x < width; x++ {
+			// 		if x == 0 && y == 0 {
+			// 			fmt.Print("[ X ]")
+			// 		} else {
+			// 			fmt.Printf("[%03d]", times[y*width+x] & (0xffff<<16)>>16)
+			// 		}
+			// 	}
+			// 	fmt.Println()
+			// }
+			// time.Sleep(time.Millisecond * 30)
+		}
+	}
+	toCheck.Init()
+	if toCheck2.Len() > 0 {
+		calculate(types, times, toCheck2, width, height, tx, ty)
+	}
+}
+
+const (
+	neither      = 1 // for wet and narrow
+	torch        = 2 // for rocky and narrow
+	climbingGear = 4 // for rocky and wet
+)
+const (
+	rocky  = 0
+	wet    = 1
+	narrow = 2
+)
