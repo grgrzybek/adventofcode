a79c4a612c8876c3a2fccb39f7d47a7f043db330
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-12-01 12:47:43 +0100

    [C++] Solution for day 01

---
+++ c/src/day01.cc
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <getopt.h>
+#include <config.h>
+
+using namespace std;
+
+static const char *program_name;
+
+static const struct option longopts[] = {
+		{ "help", no_argument, NULL, 'h' },
+		{ "version", no_argument, NULL, 'v' },
+		{ "file", required_argument, NULL, 'f' }
+};
+
+static void print_help(void);
+
+int main(int argc, char *argv[]) {
+
+	program_name = argv[0];
+	ios::sync_with_stdio(false);
+
+	int optc, lose = 0;
+	ifstream *input = NULL;
+
+	while ((optc = getopt_long(argc, argv, "hvf:", longopts, NULL)) != -1) {
+		switch (optc) {
+		case 'f':
+			input = new ifstream(optarg, ios_base::in);
+			break;
+		case 'h':
+			print_help();
+			return EXIT_SUCCESS;
+		case 'v':
+			printf("%s\n", PACKAGE_STRING);
+			return EXIT_SUCCESS;
+		default:
+			lose = 1;
+		}
+	}
+
+	if (lose || optind < argc || !input) {
+		if (optind < argc) {
+			cerr << program_name << ": extra operand: " << argv[optind] << endl;
+		}
+		cerr << "Try `" << program_name << " --help' for more information." << endl;
+		return EXIT_FAILURE;
+	}
+
+	string line;
+
+	int total_mass = 0;
+	int module_mass = 0;
+
+	int fuel1 = 0;
+	int fuel2 = 0;
+
+	while (*input) {
+		getline(*input, line);
+		if (line.length()) {
+			istringstream iss(line);
+			iss >> module_mass;
+			total_mass += module_mass;
+			int _f = (module_mass / 3) - 2;
+			fuel1 += _f;
+
+			int added_mass = _f;
+			for (;;) {
+				int _f = (added_mass / 3) - 2;
+				if (_f > 0) {
+					added_mass = _f;
+					fuel2 += _f;
+				} else {
+					break;
+				}
+			}
+
+		}
+	}
+
+	delete input;
+
+	cout << "Answer 1: " << fuel1 << endl;
+	cout << "Answer 2: " << fuel1 + fuel2 << endl;
+
+	return EXIT_SUCCESS;
+}
+
+static void print_help(void) {
+	cout << "Usage: " << program_name << " [OPTION]...\n";
+
+	cout << "Day 01.\n\n";
+
+	cout << "-h, --help      display this help and exit\n";
+	cout << "-v, --version   display version information and exit\n\n";
+
+	cout << "-f, --file=FILE puzzle input (file)\n\n";
+
+	cout << "Report bugs to <" << PACKAGE_BUGREPORT << "." << endl;
+}
