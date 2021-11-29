032e2c33676cc553f03eeea1c9149b2a5f5524ae
Grzegorz Grzybek <gr.grzybek@gmail.com>
2021-11-29 16:57:04 +0100

    [C++] Prepare for Advent of Code 2021

---
+++ c/include/utils/utils.h
@@ -0,0 +1,59 @@
+/*
+ *
+ * Copyright 2021 Grzegorz Grzybek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef AOC2021_UTILS_H
+#define AOC2021_UTILS_H
+
+#include <getopt.h>
+
+namespace aoc2021 {
+
+	class Options {
+		int argc;
+		char **argv;
+		const char *day_name;
+
+		bool _valid = true;
+		bool _finish = false;
+		int exit_code = EXIT_SUCCESS;
+
+		std::ifstream *input_file = nullptr;
+
+		void parse();
+
+		void print_help();
+
+	public:
+		const char *program_name = nullptr;
+
+		Options(const char *_day, int _argc, char *_argv[]);
+		~Options();
+
+		bool check();
+
+		[[nodiscard]] int result() const;
+
+		std::ifstream *file();
+	};
+
+	bool has_text(std::string &line);
+	void trim(std::string &line);
+
+}
+
+#endif //AOC2021_UTILS_H
---
+++ c/src/day01.cc
@@ -0,0 +1,59 @@
+/*
+ *
+ * Copyright 2021 Grzegorz Grzybek
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "utils/utils.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	aoc2021::Options options("Day 01", argc, argv);
+	if (!options.check())
+		return options.result();
+
+	cout << "Starting " << options.program_name << endl;
+
+	ifstream *input = options.file();
+
+	vector<int> values;
+
+	string line;
+	while (getline(*input, line)) {
+		aoc2021::trim(line);
+		int v = 0;
+
+		// number per line
+		istringstream iss(line);
+		iss >> v;
+		values.push_back(v);
+	}
+
+	// part 1
+
+	// part 2
+
+	cout << "Answer 1: " << 0 << endl;
+	cout << "Answer 2: " << 0 << endl;
+
+	return EXIT_SUCCESS;
+}
