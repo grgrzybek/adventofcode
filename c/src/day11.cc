edd0f0556b0ee22ab9bd3f482060225bebabf570
Grzegorz Grzybek <gr.grzybek@gmail.com>
2021-12-11 14:43:41 +0100

    [C++] Solution for Day 11 - PNG generation for each step

--- c/src/day11.cc
+++ c/src/day11.cc
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 
+#include <cstdio>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
+
+#include "png.h"
 
 #include "utils/utils.h"
 
@@ -29,6 +32,8 @@ int make_pool(int *map, int x, int y, int width, int height);
 void flash_surrounding(int *map, int x, int y, int width, int height);
 
 void print_map(int *map, int width, int height);
+
+void print_map_png(int *map, int width, int height, int step);
 
 int main(int argc, char *argv[]) {
     Options options("Day 11", argc, argv);
@@ -71,12 +76,14 @@ int main(int argc, char *argv[]) {
     int answer1 = 0;
     int answer2 = 0;
 
-    for (int i = 0; i < 1200; i++) {
+    int i = 0;
+    print_map_png(map1, width, height, i);
+    for (; i < 1200; i++) {
         for (int c = 0; c < width * height; c++) {
             map2[c] = map1[c] + 1;
         }
-        cout << "========== After step " << i << " ==========" << endl;
-        print_map(map1, width, height);
+//        cout << "========== After step " << i << " ==========" << endl;
+//        print_map(map1, width, height);
         while (true) {
             bool flash = false;
             for (int y = 0; y < height; y++) {
@@ -102,13 +109,18 @@ int main(int argc, char *argv[]) {
                 break;
             }
         }
+
+        print_map_png(map2, width, height, i);
+        swap(map1, map2);
+
         if (zero) {
-            answer2 = i+1;
+            answer2 = i + 1;
             break;
         }
-
-        swap(map1, map2);
     }
+    print_map_png(map1, width, height, i);
+
+    // finally, run `ffmpeg -framerate 24 -pattern_type glob -i '*.png' day11.mp4`
 
     cout << "Answer 1: " << answer1 << endl;
     cout << "Answer 2: " << answer2 << endl;
@@ -163,8 +175,56 @@ void flash_surrounding(int *map, int x, int y, int width, int height) {
 void print_map(int *map, int width, int height) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            cout << setw(1) << setfill(' ') << map[y*width + x];
+            cout << setw(1) << setfill(' ') << map[y * width + x];
         }
         cout << endl;
     }
+}
+
+int r[] { 0x20, 0x5f, 0x95, 0xa3, 0xb0, 0xbe, 0xcc, 0xd9, 0xe7, 0xf9 };
+int g[] { 0x00, 0x00, 0x06, 0x1d, 0x36, 0x4e, 0x65, 0x89, 0xb3, 0xed };
+int b[] { 0x07, 0x14, 0x1e, 0x1a, 0x16, 0x12, 0x0f, 0x0b, 0x07, 0x01 };
+
+void print_map_png(int *map, int width, int height, int step) {
+    char fn[64];
+    sprintf(fn, "image-%03d.png", step);
+    FILE *fp = fopen(fn, "wb");
+
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+
+    setjmp(png_jmpbuf(png_ptr));
+
+    png_init_io(png_ptr, fp);
+
+    const int SCALE = 20;
+
+    png_set_IHDR(png_ptr, info_ptr, width * SCALE, height * SCALE,
+            8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+            PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    png_write_info(png_ptr, info_ptr);
+
+    // Allocate memory for one row (3 bytes per pixel - RGB)
+    png_bytep row = (png_bytep) malloc(3 * width * SCALE * sizeof(png_byte));
+
+    // Write image data
+    int _x, _y;
+    for (_y = 0; _y < height * SCALE; _y++) {
+        int y = ((_y) / SCALE) ;
+        for (_x = 0; _x < width * SCALE; _x++) {
+            int x = ((_x) / SCALE);
+            row[_x * 3] = r[map[y * width + x]];
+            row[_x * 3 + 1] = g[map[y * width + x]];
+            row[_x * 3 + 2] = b[map[y * width + x]];
+        }
+        png_write_row(png_ptr, row);
+    }
+
+    // End write
+    png_write_end(png_ptr, nullptr);
+
+    fclose(fp);
+    png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+    png_destroy_write_struct(&png_ptr, (png_infopp) nullptr);
 }
