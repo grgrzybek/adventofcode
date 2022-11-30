ed037228000e459e78422630b11b9ba9f01c058f
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-11-30 08:46:36 +0100

    [C++] Solution for Day 23 - preparation

---
+++ c/src/day23.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 Grzegorz Grzybek
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "utils/utils.h"
+
+using namespace std;
+using namespace aoc2021;
+
+int main(int argc, char *argv[]) {
+    Options options("Day 23", argc, argv);
+    if (!options.check())
+        return options.result();
+
+    cout << "Starting " << options.program_name << endl;
+
+    ifstream *input = options.file();
+
+    string line;
+    vector<string> lines;
+
+    while (getline(*input, line)) {
+        trim(line);
+    }
+
+    // part 1
+
+    int answer1 = 0;
+
+    // part 2
+
+    int answer2 = 0;
+
+    cout << "Answer 1: " << answer1 << endl;
+    cout << "Answer 2: " << answer2 << endl;
+
+    return EXIT_SUCCESS;
+}
