faf5565af9d2d2c8c9e60ef03034a220aea9d3d6
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-12-08 11:57:31 +0100

    [2022/C++] Day 08 - add PNG generation

--- c/src/day08.cc
+++ c/src/day08.cc
@@ -19,11 +19,14 @@
 #include <string>
 #include <vector>
 
+#include "png.h"
+
 #include "utils/utils.h"
 
-unsigned long calculate_scenic_score(const int *forest, int row, int column, int w, int h);
-
 using namespace std;
+
+unsigned long calculate_scenic_score(int const *forest, int row, int column, int w, int h);
+void print_map_png(int const *map, int width, int height, int _c, int _r);
 
 int main(int argc, char *argv[]) {
     aoc2022::Options options("Day 08", argc, argv);
@@ -103,15 +106,19 @@ int main(int argc, char *argv[]) {
     // part 2
 
     unsigned long answer2 = 0;
+    int x, y;
 
     for (size_t r = 0; r < h; r++) {
         for (size_t c = 0; c < w; c++) {
             unsigned long distance = calculate_scenic_score(forest, (int) r, (int) c, (int) w, (int) h);
             if (distance > answer2) {
                 answer2 = distance;
+                x = (int) c;
+                y = (int) r;
             }
         }
     }
+    print_map_png(forest, (int) w, (int) h, x, y);
 
     cout << "Answer 1: " << answer1 << endl;
     cout << "Answer 2: " << answer2 << endl;
@@ -119,7 +126,7 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-unsigned long calculate_scenic_score(const int *forest, int row, int column, int w, int h) {
+unsigned long calculate_scenic_score(int const *forest, int row, int column, int w, int h) {
     int score = 1;
     int dir_score = 0;
     int tc = 0;
@@ -161,4 +168,56 @@ unsigned long calculate_scenic_score(const int *forest, int row, int column, int
     score *= dir_score;
 
     return tc == 0 ? 0 : score;
+}
+
+int r[] { 0x07, 0x14, 0x1e, 0x1a, 0x16, 0x12, 0x0f, 0x0b, 0x07, 0x01 };
+int g[] { 0x20, 0x5f, 0x95, 0xa3, 0xb0, 0xbe, 0xcc, 0xd9, 0xe7, 0xf9 };
+int b[] { 0x07, 0x14, 0x1e, 0x1a, 0x16, 0x12, 0x0f, 0x0b, 0x07, 0x01 };
+
+void print_map_png(int const *map, int width, int height, int _c, int _r) {
+    FILE *fp = fopen("image.png", "wb");
+
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+
+    setjmp(png_jmpbuf(png_ptr));
+
+    png_init_io(png_ptr, fp);
+
+    const int SCALE = 20;
+
+    png_set_IHDR(png_ptr, info_ptr, width * SCALE, height * SCALE,
+            8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+            PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    png_write_info(png_ptr, info_ptr);
+
+    // Allocate memory for one row (3 bytes per pixel - RGB)
+    png_bytep row = (png_bytep) malloc(3 * width * SCALE * sizeof(png_byte));
+
+    // Write image data
+    int _x, _y;
+    for (_y = 0; _y < height * SCALE; _y++) {
+        int y = ((_y) / SCALE) ;
+        for (_x = 0; _x < width * SCALE; _x++) {
+            int x = ((_x) / SCALE);
+            if (x == _c && y == _r) {
+                row[_x * 3] = 0x00;
+                row[_x * 3 + 1] = 0x00;
+                row[_x * 3 + 2] = 0xff;
+            } else {
+                row[_x * 3] = r[map[y * width + x]];
+                row[_x * 3 + 1] = g[map[y * width + x]];
+                row[_x * 3 + 2] = b[map[y * width + x]];
+            }
+        }
+        png_write_row(png_ptr, row);
+    }
+
+    // End write
+    png_write_end(png_ptr, nullptr);
+
+    fclose(fp);
+    png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+    png_destroy_write_struct(&png_ptr, (png_infopp) nullptr);
 }
