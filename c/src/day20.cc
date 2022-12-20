5e5939f37351f8c5a186edd3cbc8be232723ef61
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-12-20 20:03:43 +0100

    [2022/C++] Solution for Day 20 - part 2

--- c/src/day20.cc
+++ c/src/day20.cc
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <vector>
 
 #include "utils/utils.h"
 
@@ -33,7 +34,7 @@ int main(int argc, char *argv[]) {
     ifstream *input = options.file();
 
     string line;
-    list<pair<int, int>> numbers;
+    list<pair<int, long>> numbers;
 
     int idx = 0;
     while (getline(*input, line)) {
@@ -41,12 +42,12 @@ int main(int argc, char *argv[]) {
         if (!aoc2022::has_text(line)) {
             continue;
         }
-        numbers.emplace_back(idx++, stoi(line));
+        numbers.emplace_back(idx++, stol(line));
     }
 
     // part 1
 
-    int answer1 = 0;
+    long answer1 = 0;
 
     for (int i=0; i<idx; i++) {
         auto start = numbers.begin();
@@ -59,7 +60,7 @@ int main(int argc, char *argv[]) {
             pos++;
         }
         auto p = *pos;
-        int move = p.second;
+        long move = p.second;
         if (p.second == 0) {
             continue;
         }
@@ -83,11 +84,6 @@ int main(int argc, char *argv[]) {
         }
         numbers.insert(pos, make_pair(p.first, p.second));
     }
-    cout << "end state: ";
-    for (auto &pp: numbers) {
-        cout << pp.second << " ";
-    }
-    cout << "\n";
 
     auto pos = numbers.begin();
     while (pos != numbers.end()) {
@@ -110,7 +106,103 @@ int main(int argc, char *argv[]) {
 
     // part 2
 
-    int answer2 = 0;
+    long answer2 = 0;
+
+    // bring back the initial state
+    vector<pair<int, long>> v(numbers.begin(), numbers.end());
+    sort(v.begin(), v.end(), [](auto &p1, auto &p2) {
+        return p1.first < p2.first;
+    });
+    numbers.assign(v.begin(), v.end());
+
+    for (auto &p: numbers) {
+        p.second *= 811589153;
+    }
+
+//    cout << "initial list: ";
+//    for (auto &p: numbers) {
+//        cout << p.second << ", ";
+//    }
+//    cout << "\n";
+
+    for (int round = 0; round < 10; round++) {
+        for (int i=0; i<idx; i++) {
+            auto start = numbers.begin();
+            pos = start;
+            auto end = numbers.end();
+            while (pos++->first != i);
+            pos--;
+
+            auto p = *pos;
+            long move = p.second;
+            move %= ((int) numbers.size() - 1);
+//            cout << "moving " << p.second << " by " << move << "\n";
+//            if (move == 0) {
+//                continue;
+//            }
+            if (move < 0) {
+                move += ((int) numbers.size() - 1);
+            }
+            if (move != 0) {
+
+                auto next = pos;
+                next++;
+                numbers.erase(pos);
+                pos = next;
+                if (move > 0) {
+                    move++;
+                }
+                while (move-- > 0) {
+                    if (pos == end) {
+                        pos = start;
+                    }
+                    pos++;
+                }
+                while (move++ < 0) {
+                    if (pos == start) {
+                        pos = end;
+                    }
+                    pos--;
+                }
+                numbers.insert(pos, make_pair(p.first, p.second));
+            }
+//            cout << "    list now: ";
+//            for (auto &x: numbers) {
+//                cout << x.second << ", ";
+//            }
+//            cout << "\n";
+        }
+//        cout << "list after round " << (round + 1) << ": ";
+//        for (auto &p: numbers) {
+//            cout << p.second << ", ";
+//        }
+//        cout << "\n";
+    }
+
+//    cout << "final list: ";
+//    for (auto &p: numbers) {
+//        cout << p.second << ", ";
+//    }
+//    cout << "\n";
+
+    pos = numbers.begin();
+    while (pos != numbers.end()) {
+        if (pos->second == 0) {
+            break;
+        }
+        pos++;
+    }
+    pos++;
+    for (int i = 1; i <= 3000; i++) {
+        if (pos == numbers.end()) {
+            pos = numbers.begin();
+        }
+        if (i % 1000 == 0) {
+            cout << "adding: " << pos->second << "\n";
+            answer2 += pos->second;
+        }
+        pos++;
+    }
 
     cout << "Answer 1: " << answer1 << endl;
     cout << "Answer 2: " << answer2 << endl;
