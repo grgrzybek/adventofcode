dc020175eddec45142ab61165fe42de62b368c50
Grzegorz Grzybek <gr.grzybek@gmail.com>
2022-12-21 12:27:27 +0100

    [2022/C++] Solution for Day 21 - part 2

--- c/src/day21.cc
+++ c/src/day21.cc
@@ -29,8 +29,11 @@ struct monkey {
     bool has_value = false;
     long value;
     char operation;
+    string parent;
     string monkey1;
     string monkey2;
+
+    int where_is_human = 0; // 1: left, 2: right
 
     void print() const {
         cout << name << ": ";
@@ -42,8 +45,8 @@ struct monkey {
     }
 };
 
-void evaluate(monkey *m, map<string, monkey*> &herd) { // NOLINT(misc-no-recursion)
-    if (m->has_value) {
+void evaluate(monkey *m, map<string, monkey *> &herd) { // NOLINT(misc-no-recursion)
+    if (m->monkey1.empty()) {
         return;
     }
     monkey *m1 = herd[m->monkey1];
@@ -54,27 +57,38 @@ void evaluate(monkey *m, map<string, monkey*> &herd) { // NOLINT(misc-no-recursi
     switch (m->operation) {
         case '+':
             m->value = m1->value + m2->value;
-            cout << m1->value << "+" << m2->value << " = " << m->value << "\n";
+//            cout << m1->value << "+" << m2->value << " = " << m->value << "\n";
             break;
         case '-':
             m->value = m1->value - m2->value;
-            cout << m1->value << "-" << m2->value << " = " << m->value << "\n";
+//            cout << m1->value << "-" << m2->value << " = " << m->value << "\n";
             break;
         case '*':
             m->value = m1->value * m2->value;
-            cout << m1->value << "*" << m2->value << " = " << m->value << "\n";
+//            cout << m1->value << "*" << m2->value << " = " << m->value << "\n";
             break;
         case '/':
             if (m1->value % m2->value != 0) {
                 cout << "!!!\n";
             }
             m->value = m1->value / m2->value;
-            cout << m1->value << "/" << m2->value << " = " << m->value << "\n";
+//            cout << m1->value << "/" << m2->value << " = " << m->value << "\n";
             break;
         default:
             break;
     }
-    m->has_value = true;
+}
+
+void set_parent(monkey *m, map<string, monkey *> &herd) { // NOLINT(misc-no-recursion)
+    if (m->has_value) {
+        return;
+    }
+    monkey *m1 = herd[m->monkey1];
+    monkey *m2 = herd[m->monkey2];
+    m1->parent = m->name;
+    m2->parent = m->name;
+    set_parent(m1, herd);
+    set_parent(m2, herd);
 }
 
 int main(int argc, char *argv[]) {
@@ -89,17 +103,11 @@ int main(int argc, char *argv[]) {
     string line;
     list<monkey> monkeys;
 
-    int idx = 0;
     while (getline(*input, line)) {
         aoc2022::trim(line);
         if (!aoc2022::has_text(line)) {
             continue;
         }
-
-        // root: pppw + sjmn
-        // dbpl: 5
-        // cczh: sllz + lgvd
-        // ...
 
         monkey m;
         string::size_type pos = line.find(':');
@@ -121,11 +129,9 @@ int main(int argc, char *argv[]) {
         monkeys.emplace_back(std::move(m));
     }
 
-    map<string, monkey*> herd;
+    map<string, monkey *> herd;
     for (auto &m: monkeys) {
         herd[m.name] = &m;
-//        m.print();
-//        cout << "\n";
     }
 
     // part 1
@@ -137,7 +143,73 @@ int main(int argc, char *argv[]) {
 
     // part 2
 
-    int answer2 = 0;
+    set_parent(m, herd);
+
+    auto h = herd["humn"];
+    while (h->name != "root") {
+        auto p = herd[h->parent];
+        if (p->monkey1 == h->name) {
+            p->where_is_human = 1;
+        } else {
+            p->where_is_human = 2;
+        }
+        h = p;
+    }
+
+    long answer2 = m->value;
+
+    m = herd["root"];
+    evaluate(m, herd);
+    long changed_value = 0L;
+    while (true) {
+        if (m->name == "humn") {
+            m->value = changed_value;
+            answer2 = changed_value;
+            break;
+        }
+        long v1 = herd[m->monkey1]->value;
+        long v2 = herd[m->monkey2]->value;
+        if (m->name == "root") {
+            // easy
+            changed_value = m->where_is_human == 1 ? v2 : v1;
+            cout << "the expression for " << m->name << ": " << v1 << " " << m->operation << " " << v2 << "\n";
+            cout << "changing value of " << (m->where_is_human == 1 ? "v1" : "v2")
+                 << " (" << (m->where_is_human == 1 ? herd[m->monkey1]->name : herd[m->monkey2]->name) << ") from "
+                 << (m->where_is_human == 1 ? v1 : v2) << " to " << changed_value << "\n\n";
+        } else {
+            // recalculate changed_value based on the operation
+            switch (m->operation) {
+                case '+':
+                    changed_value = m->where_is_human == 1 ? changed_value - v2 : changed_value - v1;
+                    break;
+                case '-':
+                    changed_value = m->where_is_human == 1 ? v2 + changed_value : v1 - changed_value;
+                    break;
+                case '*':
+                    changed_value = m->where_is_human == 1 ? changed_value / v2 : changed_value / v1;
+                    break;
+                case '/':
+                    changed_value = m->where_is_human == 1 ? changed_value * v2 : changed_value * v1;
+                    break;
+                default:
+                    break;
+            }
+            cout << "the expression for " << m->name << ": " << v1 << " " << m->operation << " " << v2 << "\n";
+            cout << "changing value of " << (m->where_is_human == 1 ? "v1" : "v2")
+                 << " (" << (m->where_is_human == 1 ? herd[m->monkey1]->name : herd[m->monkey2]->name) << ") from "
+                 << (m->where_is_human == 1 ? v1 : v2) << " to " << changed_value << "\n\n";
+        }
+        if (m->where_is_human == 1) {
+            // left
+            m = herd[m->monkey1];
+        } else {
+            // right
+            m = herd[m->monkey2];
+        }
+    }
+
+    m = herd["root"];
+    evaluate(m, herd);
 
     cout << "Answer 1: " << answer1 << endl;
     cout << "Answer 2: " << answer2 << endl;
