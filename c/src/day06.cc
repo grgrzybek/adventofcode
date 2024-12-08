f6e591c35fabc35d19dc90ccfb810f04876a742b
Grzegorz Grzybek <gr.grzybek@gmail.com>
2024-12-08 11:09:29 +0100

    [2024/C++] Solution for Day 06 - part 2

--- c/src/day06.cc
+++ c/src/day06.cc
@@ -16,7 +16,9 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <deque>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -35,8 +37,7 @@ int main(int argc, char *argv[]) {
 
     string line;
     int w = 0, h = 0;
-    int x, y;
-    int x1, y1;
+    int x1, y1, x2, y2;
 
     vector<string> lines;
 
@@ -51,29 +52,38 @@ int main(int argc, char *argv[]) {
     }
 
     char *board = (char *) malloc(w * h);
-    y = 0;
+    char *board2_initial = (char *) malloc(w * h);
+    int x = 0, y = 0;
     for (auto &l: lines) {
-        strncpy(board + (y++ * w), l.c_str(), w);
+        strncpy(board + (y * w), l.c_str(), w);
+        strncpy(board2_initial + (y++ * w), l.c_str(), w);
     }
     for (y = 0; y < h; y++) {
         for (x = 0; x < w; x++) {
             if (board[y * w + x] == '^') {
                 x1 = x;
                 y1 = y;
+                x2 = x;
+                y2 = y;
             }
-            cout << board[y * w + x];
+//            cout << board[y * w + x];
         }
-        cout << endl;
+//        cout << endl;
     }
 
     // part 1
 
     int answer1 = 0;
 
+    set<pair<int, int>> coords;
+
     int dx = 0;
     int dy = -1;
     while (true) {
         board[y1 * w + x1] = 'o';
+        if (!(x1 == x2 && y1 == y2)) {
+            coords.insert(make_pair(x1, y1));
+        }
         if (dx == 0 && dy == -1 && y1 == 0) {
             // up
             break;
@@ -116,10 +126,135 @@ int main(int argc, char *argv[]) {
 
     int answer2 = 0;
 
+    for (y = 0; y < h; y++) {
+        for (x = 0; x < w; x++) {
+            if (board2_initial[y * w + x] == '^' || board2_initial[y * w + x] == '.') {
+                board2_initial[y * w + x] = '\0';
+            }
+            if (board2_initial[y * w + x] == '\0') {
+                cout << '~';
+            } else {
+                cout << board2_initial[y * w + x];
+            }
+        }
+        cout << endl;
+    }
+
+    for (auto &p: coords) {
+        char *board2 = (char *) malloc(w * h);
+        memcpy(board2, board2_initial, w * h * sizeof(char));
+        board2[p.second * w + p.first] = '#';
+
+        dx = 0;
+        dy = -1;
+        // reuse x1, y1 variables - starting from remembered initial x2, y2
+        x1 = x2;
+        y1 = y2;
+        bool loop = true;
+        while (true) {
+            // was the guard here going in the same direction?
+            // 0x00001000 - north
+            // 0x00000100 - east
+            // 0x00000010 - south
+            // 0x00000001 - west
+            char mark = board2[y1 * w + x1];
+            if (dx == 0 && dy == -1) {
+                if ((mark & 0x00001000) == 0x00001000) {
+                    break;
+                } else {
+                    board2[y1 * w + x1] |= (char) 0x00001000;
+                }
+            }
+            if (dx == 0 && dy == 1) {
+                if ((mark & 0x00000010) == 0x00000010) {
+                    break;
+                } else {
+                    board2[y1 * w + x1] |= (char) 0x00000010;
+                }
+            }
+            if (dx == 1 && dy == 0) {
+                if ((mark & 0x00000100) == 0x00000100) {
+                    break;
+                } else {
+                    board2[y1 * w + x1] |= (char) 0x00000100;
+                }
+            }
+            if (dx == -1 && dy == 0) {
+                if ((mark & 0x00000001) == 0x00000001) {
+                    break;
+                } else {
+                    board2[y1 * w + x1] |= (char) 0x00000001;
+                }
+            }
+            if (dx == 0 && dy == -1 && y1 == 0) {
+                // go out up
+                loop = false;
+                break;
+            }
+            if (dx == 1 && dy == 0 && x1 == w - 1) {
+                // go out right
+                loop = false;
+                break;
+            }
+            if (dx == 0 && dy == 1 && y1 == h - 1) {
+                // go out down
+                loop = false;
+                break;
+            }
+            if (dx == -1 && dy == 0 && x1 == 0) {
+                // go out left
+                loop = false;
+                break;
+            }
+            if (!loop) {
+                break;
+            }
+            if (board2[(y1 + dy) * w + x1 + dx] == '#') {
+                // turn right
+                if (dx == 0) {
+                    dx = -dy;
+                    dy = 0;
+                } else {
+                    dy = dx;
+                    dx = 0;
+                }
+            } else {
+                // move
+                x1 += dx;
+                y1 += dy;
+            }
+        }
+
+        if (loop) {
+//            cout << "loop for " << p.first << ":" << p.second << endl;
+            answer2++;
+//            for (y = 0; y < h; y++) {
+//                for (x = 0; x < w; x++) {
+//                    if (board2_initial[y * w + x] == '^') {
+//                        cout << "^";
+//                    }
+//                    if (x == p.first && y == p.second) {
+//                        cout << 'X';
+//                    } else if (board2[y * w + x] == '#') {
+//                        cout << '#';
+//                    } else if (board2[y * w + x] != '\0') {
+//                        cout << "o";
+//                    } else {
+//                        cout << ".";
+//                    }
+//                }
+//                cout << endl;
+//            }
+        }
+
+        free(board2);
+    }
+
     cout << "Answer 1: " << answer1 << endl;
     cout << "Answer 2: " << answer2 << endl;
 
     free(board);
+    free(board2_initial);
 
     return EXIT_SUCCESS;
 }
